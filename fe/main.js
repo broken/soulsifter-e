5f664e1b5e18f5c5f01957f81143d1248ae5cb6b
dogatech <dogatech@dogatech.home>
2023-03-02 16:00:57 -0800

    activate global ss custom native addon

--- fe/main.js
+++ fe/main.js
@@ -11,7 +11,8 @@ const createWindow = () => {
     height: 600,
     webPreferences: {
       // preload: path.join(__dirname, 'preload.js'),
-      nodeIntegration: true
+      nodeIntegration: true,
+      contextIsolation: false
     }
   })
 
