9b2f549d4957586d695a8dceb01dfc9e2a110059
dogatech <dogatech@dogatech.home>
2024-07-22 00:25:36 -0700

    auto-dragging a song also selects it

--- fe/src/song-list-item.js
+++ fe/src/song-list-item.js
@@ -193,12 +193,10 @@ class SongListItem extends SettingsMixin(WaveformUtilMixin(LitElement)) {
 
   dragSongToDeckA(e) {
     this.dragSongTo(this.settings.getString('dragAndDrop.deckLeftX'), this.settings.getString('dragAndDrop.deckLeftY'));
-    e.stopPropagation();
   }
 
   dragSongToDeckB(e) {
     this.dragSongTo(this.settings.getString('dragAndDrop.deckRightX'), this.settings.getString('dragAndDrop.deckRightY'));
-    e.stopPropagation();
   }
 
   dragSongTo(x, y) {
